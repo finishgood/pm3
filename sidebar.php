3babfcc5387874e1bb8cfae17682cf6b5b7a166f
Rizal A <rizal.fg.ikk@gmail.com>
2026-08-02 17:20:40 +0700

    tambah container drawing

--- sidebar.php
+++ sidebar.php
@@ -162,6 +162,31 @@
                                   </a>
                               </li>
                           </ul>
+                          <ul class="pcoded-item pcoded-left-item">
+                                <li class="pcoded-hasmenu">
+                                        <a href="javascript:void(0)" class="waves-effect waves-dark">
+                                            <span class="pcoded-micon"><i class="ti-truck"></i></span>
+                                            <span class="pcoded-mtext"  data-i18n="nav.basic-components.main">Drawing Calc.</span>
+                                            <span class="pcoded-mcaret"></span>
+                                        </a>
+                                    <ul class="pcoded-submenu">
+                                        <li>
+                                            <a href="drawingroll.html" class="waves-effect waves-dark" target="_blank">
+                                                <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
+                                                <span class="pcoded-mtext" data-i18n="nav.dash.main">Roll Drawing</span>
+                                                <span class="pcoded-mcaret"></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="drawingpallet.html" class="waves-effect waves-dark" target="_blank">
+                                                <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
+                                                <span class="pcoded-mtext" data-i18n="nav.dash.main">Pallet Drawing</span>
+                                                <span class="pcoded-mcaret"></span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
                         </div>
                   </nav>
 
